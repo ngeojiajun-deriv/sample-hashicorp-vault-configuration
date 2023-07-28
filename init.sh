213acd53ae71f725fa7e68e1a9ff3f5fae16cb5c
ngeojiajun-deriv <jia.jun@deriv.com>
2023-07-28 09:57:03 +0800

    fix a bug that cause the init script generated not usable

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ mkdir -p ./vault/data
 chmod ugo+rw ./vault/data
 cat > ./vault/data/init.sh <<EOF
 export VAULT_ADDR='http://127.0.0.1:8200'
-vault operator unseal $1
+vault operator unseal \$1
 
 EOF
